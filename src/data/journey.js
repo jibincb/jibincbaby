15d5be0e781920351741d9edeffb24f2c7ab8a2e
jibincb <Jibin@americaninference.com>
2025-04-22 12:40:55 +0530

    journey section

---
+++ src/data/journey.js
@@ -0,0 +1,23 @@
+export const journey = {
+  education: [
+    {
+      degree: "B.Tech, Computer Science Engineering",
+      institution: "AJP Abdul Kalam Technological University",
+      period: "2020–2024",
+    },
+    {
+      degree: "Primary & Secondary Education",
+      institution: "Nirmala Matha CBSE School, Eyyal",
+      period: "2006–2020",
+    },
+  ],
+  experience: [
+    {
+      title: "Software Developer Intern",
+      company: "American Inference",
+      period: "June, 2024 - April, 2025",
+      description:
+        "Worked on some real-world projects including Engaged Scholar.",
+    },
+  ],
+};
