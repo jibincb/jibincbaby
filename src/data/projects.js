557ea8b4ebc330f263495d421208cd88eb313bdc
jibincb <Jibin@americaninference.com>
2025-04-23 21:47:28 +0530

    projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,20 @@
 export const projects = [
   {
-    title: "Project 1",
-    description: "A cool app built with X",
-    tech: ["React", "Tailwind", "Node"],
-    githubUrl: "#",
+    id: "1",
+    title: "Personal Assistant for Blind People",
+    excerpt: "Self grooming AI assistant for blind people",
+    description:
+      "This project presents a comprehensive AI-powered solution aimed at enhancing accessibility and empowerment for visually impaired individuals. It tackles common challenges faced by the community, with a particular focus on self-grooming and environmental perception. At the heart of the project is a dress description feature, which helps users make independent decisions about their attire. Using advanced Visual Question Answering (VQA) models powered by Large Language Models, the system delivers real-time clothing descriptions, boosting users’ confidence and autonomy in their daily routines.\n\nIn addition to dress description, the project integrates several other intelligent features to create a more complete assistive experience. Face identification, built on DeepFace technology, allows for easy recognition of individuals, supporting both social interactions and safety. The object finder, developed using YOLOv5, helps users detect and locate items around them, improving spatial awareness. The scene description module leverages Vision Transformer (ViT) models to generate detailed insights about the user's environment.\n\nBy combining these cutting-edge AI tools, the project aims to support visually impaired users in navigating their world more independently and confidently. It demonstrates the practical impact of modern AI in real-life applications, highlighting its potential to improve the daily lives of those with visual impairments.",
+    tech: ["Django", "Transformers", "Yolo v5", "DeepFace", "ViT", "VQA"],
+    githubUrl: "https://github.com/jibincb/personal-assistance-updated",
   },
   {
-    title: "Project 2",
-    description: "A cool app built with X",
-    tech: ["React", "Tailwind", "Node"],
-    githubUrl: "#",
+    id: "2",
+    title: "HalfDayHire",
+    excerpt: "A part-time job hiring platform",
+    description:
+      "A clean and responsive React website for displaying job listings using Tailwind CSS. This website is designed to show full job details including position, company info, salary, location, and more. This project is incomplete and is intended to be a learning experience for me. The website is built using React and Tailwind CSS.",
+    tech: ["React", "Tailwind"],
+    githubUrl: "https://github.com/jibincb/halfdayhire",
   },
 ];
